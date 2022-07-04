f957fe398a9b6fe7d2edcaf449cd9e53ecb96bd6
Jitae9605 <dlwlxo3819@naver.com>
2022-07-04 10:20:49 +0900

    티켓발권창 디자인 추가

--- portfolio/WpfPortfolio/WpfMoogaBox/ViewModels/CheckReservationViewModel.cs
+++ portfolio/WpfPortfolio/WpfMoogaBox/ViewModels/CheckReservationViewModel.cs
@@ -6,6 +6,7 @@ namespace WpfMoogaBox.ViewModels
 	{
 		public CheckReservationViewModel()
 		{
+
 		}
 	}
 }
--- portfolio/WpfPortfolio/WpfMoogaBox/Views/CheckReservationView.xaml.cs
+++ portfolio/WpfPortfolio/WpfMoogaBox/Views/CheckReservationView.xaml.cs
@@ -1,11 +1,12 @@
-﻿using System.Windows.Controls;
+﻿using MahApps.Metro.Controls;
+using System.Windows.Controls;
 
 namespace WpfMoogaBox.Views
 {
 	/// <summary>
 	/// CheckReservationView.xaml에 대한 상호 작용 논리
 	/// </summary>
-	public partial class CheckReservationView : UserControl
+	public partial class CheckReservationView : MetroWindow
 	{
 		public CheckReservationView()
 		{
